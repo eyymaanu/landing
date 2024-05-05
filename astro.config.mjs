53cac6311756b76f2f6d3b38cabb42d2874474a7
eyymaanu <aquimanuel8@gmail.com>
2024-05-05 12:14:06 -0400

    seccion de profesores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import Swiper from 'swiper';
 
 // https://astro.build/config
 export default defineConfig({
